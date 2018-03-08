6083a2674c40954f0ceb5540ae8f69c3a22c445b
Jennifer Hansen <jenn.leigh.hansen@gmail.com>
2018-03-08 16:57:44 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -25,7 +25,9 @@ def select_name_and_series_subgenres_of_authors
 end 
 
 def select_series_title_with_most_human_characters
-  " SELECT series.title FROM series LEFT OUTER JOIN characters ON characters.series_id=series.id
+  " SELECT series.title FROM series 
+  LEFT OUTER JOIN characters ON characters.series_id=series.id
+  WHERE 
   ;"
 end 
 
