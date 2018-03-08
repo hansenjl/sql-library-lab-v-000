4694aff62e459ed43d7cd581752163af2139ff72
Jennifer Hansen <jenn.leigh.hansen@gmail.com>
2018-03-08 16:14:39 +0000

    automatically backed up by learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -10,21 +10,21 @@ CREATE TABLE subgenres
 (
 id INTEGER PRIMARY KEY,
 name TEXT
-)
+);
 
 CREATE TABLE authors
 (
 id INTEGER PRIMARY KEY,
 name TEXT
-)
+);
 
 CREATE TABLE books
 (
 id INTEGER PRIMARY KEY,
 title TEXT,
 year INTEGER,
-series_id, INTEGER
-)
+series_id INTEGER
+);
 
 CREATE TABLE characters 
 (
@@ -32,9 +32,9 @@ id INTEGER PRIMARY KEY,
 name TEXT,
 motto TEXT,
 species TEXT,
-series_id, INTEGER,
-author_id, INTEGER
-)
+series_id INTEGER,
+author_id INTEGER
+);
 
 CREATE TABLE 
 (
