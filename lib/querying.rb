63141905dacecc85c1af6c397bef3fd6fb37730d
Jennifer Hansen <jenn.leigh.hansen@gmail.com>
2018-03-08 16:26:10 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,3 +1,3 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "SELECT books.title, book.year FROM books WHERE "
+  "SELECT books.title, book.year FROM books WHERE series_id=1 ORDER BY year "
 end
