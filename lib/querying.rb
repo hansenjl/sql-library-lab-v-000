44e8ce326dcac130787902008b9420ad4ccc8bc5
Jennifer Hansen <jenn.leigh.hansen@gmail.com>
2018-03-08 16:42:25 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -18,6 +18,6 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT authors.name, subgenres.name FROM
+  "SELECT authors.name, subgenres.name FROM subgenres 
   ;"
 end 
