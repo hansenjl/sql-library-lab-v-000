99edc76383012b13954606760cbec727f8248b19
Jennifer Hansen <jenn.leigh.hansen@gmail.com>
2018-03-08 16:20:54 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
 INSERT INTO series
 (title, author_id, subgenre_id)
-VALUES ("Harry Potter", 1, 1)
+VALUES ("Harry Potter", 1, 1);
 
-INSERT INTO series
-(name, author_id, subgenre_id)
-VALUES ("Harry Potter", 1, 1)
+INSERT INTO authors
+(name)
+VALUES ("J.K. Rowling");
 
