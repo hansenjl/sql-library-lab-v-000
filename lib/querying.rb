e138258d59ba7a9159e87d0084cc7bd4f6948dd8
Jennifer Hansen <jenn.leigh.hansen@gmail.com>
2018-03-08 16:56:02 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -25,6 +25,8 @@ def select_name_and_series_subgenres_of_authors
 end 
 
 def select_series_title_with_most_human_characters
+  " SELECT series.title FROM series LEFT OUTER JOIN 
+  ;"
 end 
 
 # author
